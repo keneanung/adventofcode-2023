e6217992661fee774ac160b6898e4bc3846e1daa
Florian <keneanung@gmail.com>
2023-12-01 12:07:21 +0100

    Refactorings

--- adventofcode/day_one/puzzle_one.py
+++ adventofcode/day_one/puzzle_one.py
@@ -1,22 +1,21 @@
-from curses.ascii import isdigit
 from pathlib import Path
 from typing import Iterable
 
 from ..util import read_input
 
 
-def find_first_number(string_to_search: Iterable[str]) -> str:
+def __find_first_number(string_to_search: Iterable[str]) -> str:
     for character in string_to_search:
         if character.isdigit():
             return character
         
     raise Exception("No number found")
         
-def find_last_number(string_to_search: str) -> str:
-    return find_first_number(reversed(string_to_search))
+def __find_last_number(string_to_search: str) -> str:
+    return __find_first_number(reversed(string_to_search))
 
 def solve_puzzle(inp: Iterable[str]) -> int:
-    return sum([int(find_first_number(line) + find_last_number(line)) for line in inp])
+    return sum([int(__find_first_number(line) + __find_last_number(line)) for line in inp])
 
 def run_puzzle():
     lines = read_input(Path(__file__).parent / "input.txt")
--- adventofcode/day_one/puzzle_two.py
+++ adventofcode/day_one/puzzle_two.py
@@ -1,10 +1,9 @@
-from curses.ascii import isdigit
 from pathlib import Path
 from typing import Iterable
 
 from ..util import read_input
 
-NUMBERS = {
+__NUMBERS = {
     "one": "1",
     "two": "2",
     "three": "3",
@@ -16,41 +15,36 @@ NUMBERS = {
     "nine": "9"
 }
 
-NUMBER_STARTERS = [key[0] for key in NUMBERS.keys()]
+def __search_spelled_out_number(string_to_search: str) -> str | None:
+    for number_word in __NUMBERS.keys():
+        if string_to_search.startswith(number_word):
+            return __NUMBERS[number_word]
 
-def search_spelled_out_number(string_to_search: str) -> str | None:
-    if string_to_search[0] in NUMBER_STARTERS:
-        for number_word in NUMBERS.keys():
-            if string_to_search.startswith(number_word):
-                return NUMBERS[number_word]
-
-def check_for_number(index: int, character: str, string_to_search: str):
+def __check_for_number(index: int, character: str, string_to_search: str):
     if character.isdigit():
         return character
     else:
-        number = search_spelled_out_number(string_to_search[index:])
+        number = __search_spelled_out_number(string_to_search[index:])
         if number is not None:
             return number
     return None
 
-def find_first_number(string_to_search: str) -> str:
-    for (index, character) in enumerate(string_to_search):
-        number = check_for_number(index, character, string_to_search)
-        if number is not None:
-            return number
-        
-    raise Exception("No number found")
-        
-def find_last_number(string_to_search: str) -> str:
-    for index in reversed(range(len(string_to_search))):
-        number = check_for_number(index, string_to_search[index], string_to_search)
+def __find_number(index_iterable: Iterable[int], string_to_search: str):
+    for index in index_iterable:
+        number = __check_for_number(index, string_to_search[index], string_to_search)
         if number is not None:
             return number
         
     raise Exception("No number found")
 
+def __find_first_number(string_to_search: str) -> str:
+    return __find_number(range(len(string_to_search)), string_to_search)
+        
+def __find_last_number(string_to_search: str) -> str:
+    return __find_number(reversed(range(len(string_to_search))), string_to_search)
+
 def solve_puzzle(inp: Iterable[str]) -> int:
-    return sum([int(find_first_number(line) + find_last_number(line)) for line in inp])
+    return sum([int(__find_first_number(line) + __find_last_number(line)) for line in inp])
 
 def run_puzzle():
     lines = read_input(Path(__file__).parent / "input.txt")
